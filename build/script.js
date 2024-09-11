e30b59079b661539839461568f2196675c7a4fd3
elijahgott <gottelijah@gmail.com>
2024-09-11 17:16:40 -0500

    fixed mobile nav

--- build/script.js
+++ build/script.js
@@ -5,14 +5,12 @@ let isOpen = false;
 function toggleNav(){ //idk why this doesnt work at all sometimes, animations wont go
     isOpen = !isOpen;
     if(isOpen){ 
-        // mobileNav.classList.remove('animate-fadeOutLeft');
-        // mobileNav.classList.add('animate-fadeInLeft');
-        const timeOut = setTimeout(mobileNav.classList.remove('hidden'), 1200);
+        mobileNav.classList.remove('-translate-x-full');
+        mobileNav.classList.add('translate-x-0');
     }
     else{
-        // mobileNav.classList.remove('animate-fadeInLeft');
-        // mobileNav.classList.add('animate-fadeOutLeft');
-        const timeOut = setTimeout(mobileNav.classList.add('hidden'), 1200);
+        mobileNav.classList.remove('translate-x-0');
+        mobileNav.classList.add('-translate-x-full');
     }
 }
 
