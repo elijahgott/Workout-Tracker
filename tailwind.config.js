33da4e68db03239595352d57ec335bb8a2d1bab9
elijahgott <gottelijah@gmail.com>
2024-09-20 15:07:47 -0500

    Fixed issue with config, Fixed part of workouts page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./build/*.html'],
+  content: [
+    './build/*.html',
+    './build/workouts/*.html'],
   theme: {
     extend: {
       keyframes: {
