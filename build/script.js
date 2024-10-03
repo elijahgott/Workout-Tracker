44a795e6567fd8e1645b645fbb162cf0aa2399d3
elijahgott <gottelijah@gmail.com>
2024-10-03 10:11:57 -0500

    Began work on menu for adding workouts

--- build/script.js
+++ build/script.js
@@ -1,8 +1,11 @@
 const showMobileNav = document.querySelector('#show-mobile-nav');
 const mobileNav = document.querySelector('#mobile-nav');
 const navBlur = document.querySelector('#nav-blur');
-const body = document.querySelector('#body');
 let isNavOpen = false;
+
+const workoutBlur = document.querySelector('#workout-blur');
+const workoutMenu = document.querySelector('#workout-menu');
+let isWorkoutMenuOpen = false;
 
 function toggleNav(){
     isNavOpen = !isNavOpen;
@@ -11,18 +14,45 @@ function toggleNav(){
         mobileNav.classList.add('translate-x-0');
 
         navBlur.classList.remove('opacity-0');
+        navBlur.classList.remove('hidden');
         navBlur.classList.add('opacity-100');
+        navBlur.classList.add('absolute');
+
     }
     else{
         mobileNav.classList.remove('translate-x-0');
         mobileNav.classList.add('-translate-x-full');
 
         navBlur.classList.remove('opacity-100');
+        navBlur.classList.remove('absolute');
         navBlur.classList.add('opacity-0');
+        navBlur.classList.add('hidden');
     }
 }
 
-function addWorkout() {
+function toggleWorkoutMenu() {
+    isWorkoutMenuOpen = !isWorkoutMenuOpen;
+    if(isWorkoutMenuOpen){ 
+        workoutMenu.classList.remove('scale-0');
+        workoutMenu.classList.add('scale-100');
+
+        workoutBlur.classList.remove('opacity-0');
+        workoutBlur.classList.remove('hidden');
+        workoutBlur.classList.add('opacity-100');
+        workoutBlur.classList.add('absolute');
+    }
+    else{
+        workoutMenu.classList.remove('scale-100');
+        workoutMenu.classList.add('scale-0');
+
+        workoutBlur.classList.remove('opacity-100');
+        workoutBlur.classList.remove('absolute');
+        workoutBlur.classList.add('opacity-0');
+        workoutBlur.classList.add('hidden');
+    }
+}
+
+function submitWorkout() {
     console.log('add workout');
 }
 
@@ -32,3 +62,4 @@ function selectWorkout() {
 
 showMobileNav.addEventListener('click', toggleNav);
 navBlur.addEventListener('click', () => { isNavOpen ? toggleNav() : ''}); 
+workoutBlur.addEventListener('click', () => { isWorkoutMenuOpen ? toggleWorkoutMenu() : ''}); 
