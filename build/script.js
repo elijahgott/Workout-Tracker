06196c83d2917eba51624d224857eb73f6b6c0b9
elijahgott <gottelijah@gmail.com>
2024-10-11 13:50:31 -0500

    still need to figure out dropdown menus

--- build/script.js
+++ build/script.js
@@ -9,44 +9,49 @@ const workoutBlur = document.querySelector('#workout-blur');
 const workoutMenu = document.querySelector('#workout-menu');
 let isWorkoutMenuOpen = false;
 
+//need to delay adding -z-10 class
 function toggleNav(){
     isNavOpen = !isNavOpen;
-    if(isNavOpen){ 
+    if(isNavOpen){
+        //slide mobile nav into frame
         mobileNav.classList.remove('-translate-x-full');
         mobileNav.classList.add('translate-x-0');
-
+        //blur the background
         navBlur.classList.remove('opacity-0');
-        navBlur.classList.remove('hidden');
+        navBlur.classList.remove('-z-10');
         navBlur.classList.add('opacity-100');
-        navBlur.classList.add('absolute');
+        navBlur.classList.add('z-10');
 
     }
     else{
+        //slide mobile nav out of frame
         mobileNav.classList.remove('translate-x-0');
         mobileNav.classList.add('-translate-x-full');
-
+        //remove blur from background
         navBlur.classList.remove('opacity-100');
-        navBlur.classList.remove('absolute');
+        navBlur.classList.remove('z-10');
         navBlur.classList.add('opacity-0');
-        navBlur.classList.add('hidden');
+        navBlur.classList.add('-z-10');
     }
 }
 
 function toggleWorkoutMenu() {
     isWorkoutMenuOpen = !isWorkoutMenuOpen;
     if(isWorkoutMenuOpen){ 
+        //display workout menu
         workoutMenu.classList.remove('scale-0');
         workoutMenu.classList.add('scale-100');
-
+        //add blur to background
         workoutBlur.classList.remove('opacity-0');
         workoutBlur.classList.remove('hidden');
         workoutBlur.classList.add('opacity-100');
         workoutBlur.classList.add('absolute');
     }
     else{
+        //hide workout menu
         workoutMenu.classList.remove('scale-100');
         workoutMenu.classList.add('scale-0');
-
+        //remove blur from background
         workoutBlur.classList.remove('opacity-100');
         workoutBlur.classList.remove('absolute');
         workoutBlur.classList.add('opacity-0');
