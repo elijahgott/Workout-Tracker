70e4bc798ad7c20eefeebc07dc02230adc0dfa33
elijahgott <gottelijah@gmail.com>
2025-01-31 08:37:26 -0600

    fixed the god forsaken problems

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,35 +8,36 @@ module.exports = {
     extend: {
       keyframes: {
         "fade-in-left": {
-                    "0%": {
-                        opacity: 0,
-                        transform: "translate3d(-100%, 0, 0)",
-                    },
-                    "100%": {
-                        opacity: 1,
-                        transform: "translate3d(0, 0, 0)",
-                    },
-                },
+                  "0%": {
+                      opacity: 0,
+                      transform: "translate3d(-100%, 0, 0)",
+                  },
+                  "100%": {
+                      opacity: 1,
+                      transform: "translate3d(0, 0, 0)",
+                  },
+              },
         "fade-out-left": {
-                    "0%": {
-                        opacity: 1,
-                    },
-                    "100%": {
-                        opacity: 0,
-                        transform: "translate3d(-100%, 0, 0)",
-                    },
-                },
-      },
-      animation: {
-        fadeInLeft: 'fade-in-left 1s ease-in-out 0.25s 1',
-        fadeOutLeft: 'fade-out-left 1s ease-in-out 0.25s 1',
-      },
+                  "0%": {
+                      opacity: 1,
+                  },
+                  "100%": {
+                      opacity: 0,
+                      transform: "translate3d(-100%, 0, 0)",
+                  },
+              },
     },
+    animation: {
+      fadeInLeft: 'fade-in-left 1s ease-in-out 0.25s 1',
+      fadeOutLeft: 'fade-out-left 1s ease-in-out 0.25s 1',
+    },
+  },
   },
   variants: {
     extend: {
       display: ["group-hover"],
     },
   },
-plugins: [],
+  plugins: [],
 }
+
